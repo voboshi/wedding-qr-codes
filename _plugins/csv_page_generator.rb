c50e5474b2195241f02249eae3bf689fe4c93b18
Dan Iosif <22003158+voboshi@users.noreply.github.com>
2025-06-05 16:06:32 +0100

    link hashes to urls

--- _plugins/csv_page_generator.rb
+++ _plugins/csv_page_generator.rb
@@ -4,6 +4,8 @@ require 'base64'
 
 module Jekyll
   class CsvPage < Page
+    attr_reader :url_safe_hash
+    
     def initialize(site, base, dir, guest_data)
       @site = site
       @base = base
@@ -11,8 +13,8 @@ module Jekyll
       
       # Generate URL-safe hash from the name
       hash = Digest::SHA256.digest(guest_data['NAME'])
-      url_safe_hash = Base64.urlsafe_encode64(hash).tr('=', '')[0..15]  # Take first 16 chars for shorter URLs
-      @name = "#{url_safe_hash}.html"
+      @url_safe_hash = Base64.urlsafe_encode64(hash).tr('=', '')[0..15]  # Take first 16 chars for shorter URLs
+      @name = "#{@url_safe_hash}.html"
       
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'guest.html')
@@ -28,12 +30,26 @@ module Jekyll
     
     def generate(site)
       csv_file = File.join(site.source, 'contents.csv')
+      hash_csv_file = File.join(site.source, 'hash_names.csv')
       
       if File.exist?(csv_file)
-        CSV.foreach(csv_file, headers: true) do |row|
-          guest_data = row.to_h
-          site.pages << CsvPage.new(site, site.source, '', guest_data)
+        # Open hash_names.csv for writing
+        CSV.open(hash_csv_file, 'w', headers: ['NAME', 'HASH', 'URL'], write_headers: true) do |hash_csv|
+          CSV.foreach(csv_file, headers: true) do |row|
+            guest_data = row.to_h
+            csv_page = CsvPage.new(site, site.source, '', guest_data)
+            site.pages << csv_page
+            
+            # Write to hash_names.csv
+            hash_csv << [
+              guest_data['NAME'],
+              csv_page.url_safe_hash,
+              "https://qrs.bakers.house/#{csv_page.url_safe_hash}/"
+            ]
+          end
         end
+        
+        Jekyll.logger.info "Generated hash_names.csv with #{site.pages.length} entries"
       else
         Jekyll.logger.warn "CSV file not found at #{csv_file}"
       end
